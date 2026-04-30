39af6f0a9fec10c3273e92a991db1e9b89482833
JNAMx03 <onik.1899@gmail.com>
2026-04-29 22:37:40 -0500

    añadir sistema de puntos basico

--- screens/app/pool/StandingsScreen.tsx
+++ screens/app/pool/StandingsScreen.tsx
@@ -1,9 +1,66 @@
 import { View, Text } from 'react-native';
+import { useApp } from '../../../context/AppContext';
 
 export default function StandingsScreen() {
+
+  const { predictions } = useApp();
+
+  const results = [
+    {
+      id: '1',
+      homeScore: '2',
+      awayScore: '1',
+    },
+    {
+      id: '2',
+      homeScore: '0',
+      awayScore: '0',
+    },
+  ];
+
+  interface Prediction {
+    id: string;
+    homeScore: string;
+    awayScore: string;
+  }
+
+  const calculatePoints = () => {
+    let total = 0;
+
+    predictions.forEach((pred: Prediction) => {
+      const result = results.find((r) => r.id === pred.id);
+
+      if (!result) return;
+
+      // marcador exacto
+      if (
+        pred.homeScore === result.homeScore &&
+        pred.awayScore === result.awayScore
+      ) {
+        total += 3;
+      }
+
+      // ganador correcto
+      else if (
+        (pred.homeScore > pred.awayScore &&
+          result.homeScore > result.awayScore) ||
+        (pred.homeScore < pred.awayScore &&
+          result.homeScore < result.awayScore) ||
+        (pred.homeScore === pred.awayScore &&
+          result.homeScore === result.awayScore)
+      ) {
+        total += 1;
+      }
+    });
+
+    return total;
+  };
+
+  const totalPoints = calculatePoints();
+
   return (
     <View>
-      <Text>Clasificación</Text>
+      <Text>Puntos totales: {totalPoints}</Text>
     </View>
   );
 }
